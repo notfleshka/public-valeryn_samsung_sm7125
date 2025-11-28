62c86a148fea13146230c27bab1fd0d8a94fa5d6
Vayruz Rafli <mikhailovnakowalski@gmail.com>
2025-11-28 11:04:44 +0000

    Revert "lz4: slight reset"
    This reverts commit 90554a1b932530904bffa658d9ecc729bd0037c3.

--- lib/lz4/lz4_compress.c
+++ lib/lz4/lz4_compress.c
@@ -322,7 +322,7 @@ static FORCE_INLINE int LZ4_compress_generic(
 				*token = (BYTE)(litLength << ML_BITS);
 
 			/* Copy Literals */
-			LZ4_wildCopy(op, anchor, op + litLength);
+			LZ4_wildCopy8(op, anchor, op + litLength);
 			op += litLength;
 		}
 
@@ -627,7 +627,7 @@ static int LZ4_compress_destSize_generic(
 				*token = (BYTE)(litLength << ML_BITS);
 
 			/* Copy Literals */
-			LZ4_wildCopy(op, anchor, op + litLength);
+			LZ4_wildCopy8(op, anchor, op + litLength);
 			op += litLength;
 		}
 
@@ -707,7 +707,7 @@ _last_literals:
 		} else {
 			*op++ = (BYTE)(lastRunSize<<ML_BITS);
 		}
-		LZ4_memcpy(op, anchor, lastRunSize);
+		memcpy(op, anchor, lastRunSize);
 		op += lastRunSize;
 	}
 
